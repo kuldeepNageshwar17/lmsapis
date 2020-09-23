584ccfbe777d2602ef60f858a3574c55888eba0c
kuldeep nageshwar <nageshwar.kuldeep17@outlook.com>
2020-09-23 18:20:17 +0530

    Student API

--- controllers/student.ctrl.js
+++ controllers/student.ctrl.js
@@ -23,8 +23,7 @@ addStudent = async (req, res) => {
           $set: {
             name: req.body.name,
             email: req.body.email,
-            mobile: req.body.mobile,
-            password: req.body.password
+            mobile: req.body.mobile
           }
         }
       )
@@ -34,6 +33,64 @@ addStudent = async (req, res) => {
     return res.status(500).send(error)
   }
 }
+
+resetPassword = async (req, res) => {
+  try {
+    let { password, confirmPassword } = req.body
+    let id = req.params.id
+
+    if (password === confirmPassword) {
+      await Student.updateOne(
+        { _id: id },
+        {
+          $set: {
+            password: password
+          }
+        }
+      )
+      return res.status(200).send('password Changes Successfully')
+    }
+    return res.status(400).send('Passsword Did not match ')
+  } catch (error) {
+    return res.status(500).send(error)
+  }
+}
+UploadProfileImage = async (req,res) => {
+  try {
+    if (!req.files) {
+      res.send({
+        status: false,
+        message: 'No file uploaded'
+      })
+    } else {
+      //Use the name of the input field (i.e. "avatar") to retrieve the uploaded file
+      let file = req.files.file
+      var id = req.params.id
+      
+
+      var filename =file.name.substr(0, file.name.lastIndexOf('.')).substr(0, 10) + path.extname(file.name)
+      file.mv('./public/uploads/Profiles/'+id+filename)
+
+      await Student.updateOne(
+        { _id: id },
+        {
+          $set: {
+            profileImage:id+filename
+          }
+        }
+      )
+     return res.status(200).send({
+        status: true,
+        message: 'File is uploaded',
+        name: id+filename,
+        mimetype: file.mimetype
+      })
+    }
+  }catch (err){
+    res.status(500).send(err)
+  }
+}
+
 getStudents = async (req, res) => {
   let branchId = req.headers.branchid
   try {
@@ -47,7 +104,11 @@ getStudent = async (req, res) => {
   let id = req.params.id
   try {
     var student = await Student.findOne({ _id: id })
-    return res.status(200).send(student)
+    const { batches } = await Branch.findOne(
+      { 'batches._id': student.currentBatch },
+      { 'batches.$': 1 }
+    )
+    return res.status(200).send({ student, batches })
   } catch (error) {
     return res.status(500).send(error)
   }
@@ -66,5 +127,7 @@ module.exports = {
   addStudent,
   getStudents,
   getStudent,
-  getBatchesDdr
+  getBatchesDdr,
+  resetPassword,
+  UploadProfileImage
 }
--- routes/student.router.js
+++ routes/student.router.js
@@ -5,8 +5,10 @@ const {ROLE_LABLE } = require('../models/constants')
 
 const router = express.Router()
 router.post('/Student',auth([ROLE_LABLE.BRANCH_LABLE]), StudentCtrl.addStudent)
-router.get('/Student',auth([ROLE_LABLE.BRANCH_LABLE]), StudentCtrl.getStudents)
 router.get('/Student/:id',auth([ROLE_LABLE.BRANCH_LABLE]), StudentCtrl.getStudent)
+router.get('/Student',auth([ROLE_LABLE.BRANCH_LABLE]), StudentCtrl.getStudents)
 router.get('/Batchesddr',auth([ROLE_LABLE.BRANCH_LABLE]), StudentCtrl.getBatchesDdr)
+router.post('/StudentPasswordReset/:id',auth([ROLE_LABLE.BRANCH_LABLE]), StudentCtrl.resetPassword)
+router.post('/ChangeProfileImage/:id',auth([ROLE_LABLE.BRANCH_LABLE]), StudentCtrl.UploadProfileImage)
 
 module.exports = router
